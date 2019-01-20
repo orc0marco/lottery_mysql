423131a06efbbe0d1f7b49c4e7b761d826dc5532
mengguo <mg@kuaihuoyun.com>
2019-01-20 16:33:08 +0800

    fix

---
+++ mysqlTest2.py
@@ -0,0 +1,21 @@
+# -*-coding:utf-8-*-
+# @Author  : marco
+# @Time    : 2019-01-09 15:41
+import csv
+import pymysql
+
+
+# 打开数据库连接
+db = pymysql.connect("wwt.cggll8by8tsn.ap-northeast-1.rds.amazonaws.com", "wwt", "wwt110110", "lottery_activity")
+cursor = db.cursor()
+
+csv_reader = csv.reader(open("test.csv"))
+for row in csv_reader:
+    cursor.execute("select id from user where username = ?", str[row[0]])
+    results = cursor.fetchall()
+    print(results)
+
+
+
+
+
